f2bedd281807636038abc0a3e0afd3656c2f76d0
Hagai Kariti <hkariti@gmail.com>
2019-06-30 17:19:15 +0300

    more trials, now with smaller batches and scheduling

--- wet3/q2.py
+++ wet3/q2.py
@@ -7,7 +7,8 @@ from mountain_car_with_data_collection import MountainCarWithResetEnv
 class QLearningAgent:
     def __init__(self):
         self.game = MountainCarWithResetEnv()
-        self.theta = np.zeros((1, 18))
+        #self.theta = np.zeros((1, 18))
+        self.theta = np.zeros((1, 78))
 
         # Constants used for data standardization
         self.pos_mu = (self.game.min_position + self.game.max_position)/2
@@ -17,9 +18,9 @@ class QLearningAgent:
 
     def reset(self, state=None):
         if state:
-            self.game.reset_specific(*state)
+            return self.game.reset_specific(*state)
         else:
-            self.game.reset()
+            return self.game.reset()
 
     def next_a(self, state):
         N = np.shape(state)[0]
@@ -61,7 +62,12 @@ class QLearningAgent:
         n_s = np.zeros(s.shape)
         n_s[:, 0] = (s[:, 0] - self.pos_mu) / self.pos_sigma
         n_s[:, 1] = (s[:, 1] - self.speed_mu) / self.speed_sigma
+        #centers = [(-1.2, -0.07), (-1.2, 0.07), (0.5, -0.07), (0.5, 0.07), (0, 0)]
         centers = [(-1.2, -0.07), (-1.2, 0.07), (0.5, -0.07), (0.5, 0.07), (0, 0)]
+        centers = []
+        for i in -1.2, -0.6, 0, 0.6, 1.2:
+            for j in -0.07, -0.03, 0, 0.03, 0.07:
+                centers.append((i, j))
         n_centers = np.array([(
             (c[0] - self.pos_mu/self.pos_sigma),
             (c[1] - self.speed_mu/self.speed_sigma)) for c in centers])
@@ -94,38 +100,65 @@ class QLearningAgent:
 
         plt.show()
 
-    def gather_data(self, epsilon, iterations=10000):
-        states = np.zeros((iterations, 2))
-        actions = np.zeros((iterations, 1))
-        next_states = np.zeros((iterations, 2))
-        rewards = np.zeros((iterations, 1))
+    def train_online(self, epsilon=0.5, alpha=0.1, gamma=0.999, iterations=500):
+        state = self.reset()
+        for i in range(iterations):
+            if np.random.uniform() > epsilon:
+                rand = False
+                action = self.next_a(state.reshape((1, 2)))[0]
+            else:
+                rand = True
+                action = np.random.choice(3)
+            print(action)
+            next_state, reward, is_done, _ = self.game.step(action)
+            reward = (-1)**(1+is_done)
+            coeff = reward + gamma * self.q_max(next_state.reshape((1, 2))) - self.q(state.reshape((1, 2)), np.array([action]))
+            update_step = self.extract_features(state.reshape((1, 2)), np.array([action])) * coeff
+            old_theta = self.theta.copy()
+            self.theta += alpha * update_step
+            diff = np.max(np.abs(self.theta - old_theta))
+            state = next_state
+            print("Iteration", i, "diff", diff)
+            if is_done:
+                print("Done on iteration", i)
+                return 1
+        return 0
+
+    def gather_data(self, epsilon, iterations_per_game=200, games=5):
+        states = np.zeros((iterations_per_game*games, 2))
+        actions = np.zeros((iterations_per_game*games, 1))
+        next_states = np.zeros((iterations_per_game*games, 2))
+        rewards = np.zeros((iterations_per_game*games, 1))
         data = (states, actions, next_states, rewards)
 
-        state = np.array(self.game.state).reshape((1, 2))
-        is_done = False
-        i = None
-        for i in range(iterations):
-            if np.random.uniform() < epsilon:
-                rand = True
-                action = self.next_a(state)[0]
-            else:
-                rand = False
-                action = np.random.choice([0, 1, 2])
-            next_state, reward, is_done, _ = self.game.step(action)
-            states[i, :] = state
-            actions[i, :] = action
-            next_states[i, :] = next_state
-            rewards[i, :] = reward
-            #print("i: {}, state: {}, action: {}, next: {}, r: {}, rand: {}".format(i, state, action, next_state, reward, rand))
-            if is_done or (not is_done and i > 1000):
-                self.reset_random()
-            state = np.array(next_state).reshape((1, 2))
-        return data, is_done, i
+        was_done = False
+        data_index = 0
+        for g in range(games):
+            state = self.reset_random()
+            state = state.reshape((1, 2))
+            for i in range(iterations_per_game):
+                if np.random.uniform() > epsilon:
+                    rand = False
+                    action = self.next_a(state)[0]
+                else:
+                    rand = True
+                    action = np.random.choice(3)
+                next_state, reward, is_done, _ = self.game.step(action)
+                was_done |= is_done
+                states[data_index, :] = state
+                actions[data_index, :] = action
+                next_states[data_index, :] = next_state
+                rewards[data_index, :] = reward
+                data_index += 1
+                #print("i: {}, state: {}, action: {}, next: {}, r: {}, rand: {}".format(i, state, action, next_state, reward, rand))
+                state = np.array(next_state).reshape((1, 2))
+                if is_done:
+                    break
+        return data, was_done, data_index
 
-    def train_step(self, alpha, data, batch_size=500, gamma=0.99):
+    def train_step(self, alpha, data, batch_size=100, gamma=0.99):
         data_length = data[0].shape[0]
-        batch_indices = list(range(batch_size))
-        np.random.shuffle(batch_indices)
+        batch_indices = np.random.randint(0, data_length, batch_size)
 
         states = data[0][batch_indices]
         actions = data[1][batch_indices]
@@ -140,41 +173,45 @@ class QLearningAgent:
 
     def reset_random(self):
         init_state = (np.random.uniform(-1.2, 0.6), np.random.uniform(-0.07, 0.07))
-        self.reset(init_state)
+        return self.reset(init_state)
 
-    def train(self, init_state=None, init_epsilon=0.1, init_alpha=0.1, max_iterations=100):
+    def train(self, init_state=None, init_epsilon=1, init_alpha=1, max_iterations=100):
+        #_, states, actions, rewards, next_states = q1.lspi_data_sample(10000)
+        #data = (states, actions.reshape((10000, 1)), next_states, rewards)
+        alpha = init_alpha
+        epsilon = init_epsilon
         for i in range(max_iterations):
-            self.reset_random()
-            data, is_done, max_ind = self.gather_data(init_epsilon)
+            data, is_done, max_ind = self.gather_data(epsilon)
 
             if not is_done:
                 print("Didn't finish, num", i)
 
-            data = (data[0][:max_ind + 1, :],
-                    data[1][:max_ind + 1, :],
-                    data[2][:max_ind + 1, :],
-                    data[3][:max_ind + 1, :])
-            for i in range(4):
-                new_theta = self.train_step(init_alpha, data)
-                theta_diff = new_theta - self.theta
-                self.theta = new_theta
+            data = (data[0][:max_ind, :],
+                    data[1][:max_ind, :],
+                    data[2][:max_ind, :],
+                    data[3][:max_ind, :])
+            old_theta = self.theta
+            for j in range(10):
+                self.theta = self.train_step(alpha, data)
+            theta_diff = self.theta - old_theta
 
             diff_max = np.max(np.abs(theta_diff))
-            print("theta_new - theta (max) =", diff_max)
+            print("Iter", i, "alpha", alpha, "ep", epsilon, "theta_new - theta (max) =", diff_max)
+            epsilon = 0.9 * epsilon
+            alpha = 0.9 * alpha
 
             #if diff_max <= 0.001:
             #    print("Converged!")
             #    return
 
     def play(self):
-        self.reset()
-
+        state = self.reset().reshape((1,2))
         for i in range(1000):
-            state = np.array(self.game.state).reshape((1, 2))
             action = int(self.next_a(state))
             next_state, reward, is_done, _ = self.game.step(action)
             self.game.render()
             state = np.array(next_state).reshape((1,2))
             if is_done:
-                return
+                break
+        self.game.close()
 
